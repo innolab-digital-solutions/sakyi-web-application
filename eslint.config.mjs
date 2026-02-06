0b2b58fd3f16e33e4873b6b91fc678c70ecc73ba
aung-thu-zaw <aungthuzaw223@gmail.com>
2026-02-06 15:31:08 +0630

    SHW-8 solved the formatting check error on ci

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,19 +1,19 @@
-import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals";
-import nextTs from "eslint-config-next/typescript";
-import prettier from "eslint-config-prettier/flat";
+import { defineConfig, globalIgnores } from 'eslint/config';
+import nextVitals from 'eslint-config-next/core-web-vitals';
+import nextTs from 'eslint-config-next/typescript';
+import prettier from 'eslint-config-prettier/flat';
 
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
   prettier,
   globalIgnores([
-    ".cursor/**",
-    "node_modules/**",
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
+    '.cursor/**',
+    'node_modules/**',
+    '.next/**',
+    'out/**',
+    'build/**',
+    'next-env.d.ts',
   ]),
 ]);
 
