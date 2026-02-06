6d48ec91ed086238d8b2447bad4e616e33e11d73
aung-thu-zaw <aungthuzaw223@gmail.com>
2026-02-06 12:39:42 +0630

    SHW-8 setup next js 16 mcp dev tools and add ignore

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -7,7 +7,8 @@ const eslintConfig = defineConfig([
   ...nextTs,
   // Override default ignores of eslint-config-next.
   globalIgnores([
-    // Default ignores of eslint-config-next:
+    ".cursor/**",
+    "node_modules/**",
     ".next/**",
     "out/**",
     "build/**",
