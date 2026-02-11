41c13cd8e35c5c2367c79a9af90a4c8a9033e32c
aung-thu-zaw <aungthuzaw223@gmail.com>
2026-02-11 13:42:47 +0630

    SHW-12 fixed formatting check ci error

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig, globalIgnores } from 'eslint/config';
 import nextVitals from 'eslint-config-next/core-web-vitals';
 import nextTs from 'eslint-config-next/typescript';
 import prettier from 'eslint-config-prettier/flat';
-import query from '@tanstack/eslint-plugin-query'
+import query from '@tanstack/eslint-plugin-query';
 
 const eslintConfig = defineConfig([
   ...nextVitals,
