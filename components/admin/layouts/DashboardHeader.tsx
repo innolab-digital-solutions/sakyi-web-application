391350ebfc5c94fe2c1aa4c7a29968d6081d0e59
aung-thu-zaw <aungthuzaw223@gmail.com>
2026-02-16 23:09:52 +0630

    SHW-15 add hard coded breadcrumb on the dashboard header

--- components/admin/layouts/DashboardHeader.tsx
+++ components/admin/layouts/DashboardHeader.tsx
@@ -1,12 +1,22 @@
 import { Bell } from 'lucide-react';
+import Link from 'next/link';
 
 import { Button } from '@/components/ui/button';
 import { Separator } from '@/components/ui/separator';
 import { SidebarTrigger } from '@/components/ui/sidebar';
+import {
+  Breadcrumb,
+  BreadcrumbList,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from '@/components/ui/breadcrumb';
+import { PATHS } from '@/config/paths';
 
 const DashboardHeader = () => {
   return (
-    <header className="border-border/80 sticky top-0 z-50 w-full border-b bg-background">
+    <header className="border-border/80 bg-background sticky top-0 z-50 w-full border-b">
       <div className="flex h-16 items-center justify-between px-5">
         {/* Sidebar section with trigger and potential breadcrumb */}
         <div className="flex h-5 items-center">
@@ -19,7 +29,28 @@ const DashboardHeader = () => {
           <Separator orientation="vertical" className="mx-3 hidden sm:block" />
 
           {/* Placeholder for dynamic breadcrumb navigation (shown on sm and up) */}
-          <div className="hidden sm:block">{/* <DynamicBreadcrumb /> */}</div>
+          <div className="hidden sm:block">
+            <Breadcrumb>
+              <BreadcrumbList>
+                <BreadcrumbItem>
+                  <BreadcrumbPage className="text-foreground font-medium">
+                    Control Panel
+                  </BreadcrumbPage>
+                </BreadcrumbItem>
+                <BreadcrumbSeparator />
+                <BreadcrumbItem>
+                  <BreadcrumbLink asChild>
+                    <Link
+                      href={PATHS.ADMIN.DASHBOARD}
+                      className="text-foreground hover:text-accent! font-medium"
+                    >
+                      Dashboard
+                    </Link>
+                  </BreadcrumbLink>
+                </BreadcrumbItem>
+              </BreadcrumbList>
+            </Breadcrumb>
+          </div>
         </div>
 
         <div className="flex items-center gap-4">
