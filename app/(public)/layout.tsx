d236bd1ede8ef378720df31e2484f33897c2311d
aung-thu-zaw <aungthuzaw223@gmail.com>
2026-02-20 11:22:47 +0630

    SHW-24 setup aos for the public pages and fixed the hydration mismatch error with AOS

--- app/(public)/layout.tsx
+++ app/(public)/layout.tsx
@@ -1,13 +1,16 @@
 import { PropsWithChildren } from 'react';
 import Navbar from '@/components/public/layouts/Navbar';
 import Footer from '@/components/public/layouts/Footer';
+import AOSProvider from '@/components/shared/AOSProvider';
 
 export default function PublicLayout({ children }: PropsWithChildren) {
   return (
-    <div className="min-h-screen">
-      <Navbar />
-      {children}
-      <Footer />
-    </div>
+    <AOSProvider>
+      <div className="min-h-screen flex-1">
+        <Navbar />
+        {children}
+        <Footer />
+      </div>
+    </AOSProvider>
   );
 }
