0ba8f57a37e68c93a45dee17d9644e3dedb0a98b
kyawMHtet <kyawmhtet23@gmail.com>
2026-04-03 17:31:54 +0700

    SHW-66 fix prettier format in domains/units/services

--- domains/units/services/index.ts
+++ domains/units/services/index.ts
@@ -5,4 +5,4 @@ export {
   getUnits,
   updateUnit,
 } from './admin.service';
-export { getUnitsLookup,type UnitLookup } from './lookup.service';
+export { type UnitLookup, getUnitsLookup } from './lookup.service';
